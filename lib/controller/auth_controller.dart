3fe532ce6344d7e8491661589685336f0dbb6ba9
DeepGandhi07 <deepgandhi9887@gmail.com>
2023-04-27 23:48:50 +0530

    commit

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,11 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce_shopping/consts/consts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
   // Login method
   Future<UserCredential?> loginMethod({email, password, context}) async {
-    UserCredential userCredential;
+    UserCredential? userCredential;
 
     try {
       await auth.signInWithEmailAndPassword(email: email, password: password);
@@ -15,5 +17,35 @@ class AuthController extends GetxController {
         msg: e.toString(),
       );
     }
+    return userCredential;
+  }
+
+  //signUp method
+  Future<UserCredential?> signUpMethod({email, password, context}) async {
+    UserCredential? userCredential;
+
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      VxToast.show(
+        context,
+        msg: e.toString(),
+      );
+    }
+    return userCredential;
+  }
+
+  // Storing data method
+
+  storeUserData({name, password, email, imageUrl}) async {
+    DocumentReference store =
+        await firestore.collection(usersCollection).doc(currentUser!.uid);
+    store.set({
+      'name': name,
+      'password': password,
+      'email': email,
+      'imageUrl': '',
+    });
   }
 }
