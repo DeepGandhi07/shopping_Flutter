6c67443702845665d69cc3ecfcd67d203e0b64ea
DeepGandhi07 <deepgandhi9887@gmail.com>
2023-04-28 22:34:04 +0530

    commit

--- lib/views/Profile_screen/edit_profile.dart
+++ lib/views/Profile_screen/edit_profile.dart
@@ -1,5 +1,7 @@
 import 'package:ecommerce_shopping/consts/consts.dart';
 import 'package:ecommerce_shopping/widgets_common/bg_widget.dart';
+import 'package:ecommerce_shopping/widgets_common/customtextField.dart';
+import 'package:ecommerce_shopping/widgets_common/login_button.dart';
 import 'package:flutter/material.dart';
 
 class EditProfile extends StatelessWidget {
@@ -15,14 +17,48 @@ class EditProfile extends StatelessWidget {
           ),
         ),
         body: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(imgProfile2, width: 100, fit: BoxFit.cover)
                 .box
                 .clip(Clip.antiAlias)
                 .roundedFull
                 .make(),
+            10.heightBox,
+            loginButton(
+              onPress: () {},
+              color: redColor,
+              textColor: whiteColor,
+              title: Text('change'),
+            ),
+            Divider(),
+            20.heightBox,
+            customColorField(
+              hint: nameHint,
+              title: name,
+              isPass: false,
+            ),
+            customColorField(
+              hint: passwordHint,
+              title: password,
+              isPass: true,
+            ),
+            20.heightBox,
+            loginButton(
+              onPress: () {},
+              color: redColor,
+              textColor: whiteColor,
+              title: Text('Save'),
+            ),
           ],
-        ),
+        )
+            .box
+            .padding(EdgeInsets.all(16))
+            .margin(EdgeInsets.only(top: 50, left: 12, right: 12))
+            .rounded
+            .white
+            .shadowSm
+            .make(),
       ),
     );
   }
